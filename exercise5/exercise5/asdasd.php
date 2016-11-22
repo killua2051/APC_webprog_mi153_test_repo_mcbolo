2d856f22380015d8b7c1e5719a512ba3812b2751
Maverick Bolo <killua2051@gmail.com>
2016-11-22 13:08:18 +0800

    Edited code.

---
+++ exercise5/exercise5/asdasd.php
@@ -0,0 +1,163 @@
+<html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid white;
+    color: seagreen;
+    font-size: 25;
+    
+
+}
+body {
+    background-color: black;
+}
+
+h1 {
+    color: steelblue;
+    text-align: center;
+    font-size: 40;
+}
+
+.error {color: #FF0000;}
+
+</style>
+</head>
+<body>
+
+<h1>Maverick Jave C. Bolo</h1>
+<center><img src = "wsaLOI__.jpg" style="width:400px;height:400px"></center>
+<table>
+<tr>
+    <td>Nickname: </td>
+    <td>Mav</td>
+  </tr>
+  
+<tr>
+    <td>Hobbies: </td>
+    <td>Skateboarding, Playing Online Games, Cycling, Training Mixed Martial Arts</td>
+  </tr>
+  <tr>
+    <td>Interests: </td>
+    <td>Bikes, Computers, Skateboards, Mixed Martial Arts</td>
+  </tr>
+  <tr>
+    <td>Favorite Websites: </td>
+    <td>None</td>
+
+</table>
+
+<h1>Trivias about me</h1>
+
+<font color="SeaGreen" size="5"><p id="demo1">1. Where does your name come from?</p>
+
+<button type="button" onclick="document.getElementById('demo1').innerHTML = 'From the movie Top Gun.'">Click Me!</button>
+
+
+<font color="SeaGreen" size="5"><p id="demo2">2. What is your main role in League of Legends?</p>
+
+<button type="button" onclick="document.getElementById('demo2').innerHTML = 'Mid or AD Carry'">Click Me!</button>
+
+
+<font color="SeaGreen" size="5"><p id="demo3">3. What is your main martial art? </p></font>
+
+<button type="button" onclick="document.getElementById('demo3').innerHTML = 'Sayaw ng Kamatayan (Dance of Death)'">Click Me!</button>
+
+    <?php
+    // define variables and set to empty values
+    $nameErr = $emailErr = $genderErr = $websiteErr = "";
+    $name = $email = $gender = $comment = $website = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "Name is required";
+        } else {
+            $name = test_input($_POST["name"]);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+                $nameErr = "Only letters and white space allowed";
+            }
+        }
+
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is required";
+        } else {
+            $email = test_input($_POST["email"]);
+            // check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email format";
+            }
+        }
+
+        if (empty($_POST["website"])) {
+            $website = "";
+        } else {
+            $website = test_input($_POST["website"]);
+            // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+                $websiteErr = "Invalid URL";
+            }
+        }
+
+        if (empty($_POST["comment"])) {
+            $comment = "";
+        } else {
+            $comment = test_input($_POST["comment"]);
+        }
+
+        if (empty($_POST["gender"])) {
+            $genderErr = "Gender is required";
+        } else {
+            $gender = test_input($_POST["gender"]);
+        }
+    }
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+
+    <h2>PHP Form Validation Example</h2>
+    <p><span class="error">* required field.</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        Name: <input type="text" name="name" value="<?php echo $name;?>">
+        <span class="error">* <?php echo $nameErr;?></span>
+        <br><br>
+        E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+        <span class="error">* <?php echo $emailErr;?></span>
+        <br><br>
+        Website: <input type="text" name="website" value="<?php echo $website;?>">
+        <span class="error"><?php echo $websiteErr;?></span>
+        <br><br>
+        Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+        <br><br>
+        Gender:
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+        <span class="error">* <?php echo $genderErr;?></span>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+    echo "<h2>Your Input:</h2>";
+    echo $name;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $website;
+    echo "<br>";
+    echo $comment;
+    echo "<br>";
+    echo $gender;
+    ?>
+
+
+
+
+
+</body>
+</html>
+
